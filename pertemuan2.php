1551d61ccfb958e9e3cceeee0e432de2ef0f25d9
Rafif Faisal <faisalghozi458@gmail.com>
2023-04-25 19:43:24 +0700

    menambahkan pertemuan2

---
+++ pertemuan2.php
@@ -0,0 +1,18 @@
+<?php
+
+    // Sintaks PHP
+
+    /*
+    Standar Output
+    echo 
+    print_r
+    var_dump()
+    */
+
+    $string="Hello World!";
+    $array=['pisang','apel','nanas']; //index atau hitungannya dari 0
+    echo $string;
+    print_r($array[0]);
+    var_dump($array);
+
+?>
